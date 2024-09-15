89b20fabb1e311802c9d7e993596020b2d5f912d
Abhay Hitendra Dusane <abhayhd_Chrome@skncoe365.onmicrosoft.com>
2024-09-15 15:24:50 +0530

    basic()

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import Papa from "papaparse";
+import { useState, useEffect } from "react";
+import salaries from "./salaries.csv";
 function App() {
+  const [data, setData] = useState([]);
+  // const handleFileUpload = (e) => {
+  //   const file = e.target.files[0];
+  //   Papa.parse(file, {
+  //     header: true,
+  //     complete: (results) => {
+  //       setData(results.data);
+  //     },
+  //   });
+  // };
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(salaries);
+      const reader = response.body.getReader();
+      const result = await reader.read();
+      const decoder = new TextDecoder("utf-8");
+      const csvData = decoder.decode(result.value);
+      const parsedData = Papa.parse(csvData, {
+        header: true,
+        skipEmptyLines: true,
+      }).data;
+      setData(parsedData);
+    };
+    fetchData();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <input type="file" accept=".csv" onChange={handleFileUpload} /> */}
+      {data.length ? (
+        <table className="table">
+          <thead>
+            <tr>
+              <th>work_year</th>
+              <th>experience_level</th>
+              <th>employment_type</th>
+              <th>job_title</th>
+              <th>salary</th>
+              <th>salary_currency</th>
+              <th>salary_in_USD</th>
+              <th>employee_residence</th>
+              <th>remote_ratio</th>
+              <th>company_location</th>
+              <th>company_size</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((row, index) => (
+              <tr key={index}>
+                <td>{row.work_year}</td>
+                <td>{row.experience_level}</td>
+                <td>{row.employment_type}</td>
+                <td>{row.job_title}</td>
+                <td>{row.salary}</td>
+                <td>{row.salary_currency}</td>
+                <td>{row.salary_in_usd}</td>
+                <td>{row.employee_residence}</td>
+                <td>{row.salary_in_usd}</td>
+                <td>{row.employee_residence}</td>
+                <td>{row.remote_ratio}</td>
+                <td>{row.company_location}</td>
+                <td>{row.company_size}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : null}
     </div>
   );
 }
